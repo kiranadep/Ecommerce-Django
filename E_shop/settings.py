9d08eda1a09950f46fb4030c67e461b3542dc3f0
kiranadep <93147241+kiranadep@users.noreply.github.com>
2024-08-04 22:12:06 +0900

    settings.py

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'E_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
